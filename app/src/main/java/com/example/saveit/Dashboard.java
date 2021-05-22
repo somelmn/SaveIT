c71d9591f674764412e3793efdcdcc0dc8129ba4
ogedayyazici <ogedayyazici@gmail.com>
2021-05-22 21:52:56 +0300

    a very basic graph in dashboard

--- app/src/main/java/com/example/saveit/Dashboard.java
+++ app/src/main/java/com/example/saveit/Dashboard.java
@@ -6,6 +6,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Dashboard extends AppCompatActivity {
     DrawerLayout drawerLayout;
 
@@ -15,6 +19,16 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         drawerLayout= findViewById(R.id.drawer_layout);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     public void ClickMenu(View view){
