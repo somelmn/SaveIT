767c4c8cc17284e41df7f77b2ead63665c95cc2e
Ece Öğretmen <57140620+eceogretmen@users.noreply.github.com>
2021-05-27 23:53:04 +0300

    Bills page pt2 (needs work)

--- app/src/main/java/com/example/saveit/GasBill.java
+++ app/src/main/java/com/example/saveit/GasBill.java
@@ -1,14 +1,93 @@
 package com.example.saveit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class GasBill extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    MeowBottomNavigation bottomNavigation;
+    TextView tname,temail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gas_bill);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        tname = findViewById(R.id.nav_name);
+        temail = findViewById(R.id.nav_email);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getEmail();
+        temail.setText(uid);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.orderByChild("email").equalTo(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    String name = snapshot.child("fullName").getValue().toString();
+                    tname.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    public void ClickElectricity(View view){
+        MainActivity.redirectActivity(this,ElectricityBill.class);
+    }
+    public void ClickWater(View view){
+        MainActivity.redirectActivity(this,WaterBill.class);
+    }
+    public void ClickGas(View view){
+        recreate();
+    }
+
+    public void ClickMenu(View view){
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view){
+        MainActivity.closeDrawer(drawerLayout);
+    }
+    public void ClickHome(View view){
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+    public void ClickBills(View view){
+        MainActivity.redirectActivity(this,Bills.class);
+    }
+    public void ClickAboutUs(View view){
+        MainActivity.redirectActivity(this,AboutUs.class);
+
+    }
+    public void ClickSettings(View view){
+        MainActivity.redirectActivity( this,Preference.class);
+    }
+    public void ClickLogout(View view){
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        MainActivity.closeDrawer(drawerLayout);
     }
 }
