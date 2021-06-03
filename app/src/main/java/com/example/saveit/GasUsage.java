e50893a73192c77f30cddd5258fb0585db52e5b5
Sumiya Elmnefi <49877974+somelmn@users.noreply.github.com>
2021-06-04 00:01:21 +0300

    Usage page updated for water and gas!

--- app/src/main/java/com/example/saveit/GasUsage.java
+++ app/src/main/java/com/example/saveit/GasUsage.java
@@ -10,7 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,10 +24,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class GasUsage extends AppCompatActivity {
     DrawerLayout drawerLayout;
-    MeowBottomNavigation bottomNavigation;
     TextView tname,temail;
+    private static String TAG ="GasUsage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +73,55 @@ public class GasUsage extends AppCompatActivity {
             LinearLayout li=(LinearLayout)findViewById(R.id.nav_drawer);
             li.setBackgroundResource(R.color.white);
         }
+
+         BarChart barChart = findViewById(R.id.graph);
+
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        List<String> xAxisValues = new ArrayList<>(Arrays.asList("Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"));
+        List<BarEntry> usageEntries = getUsageEntries();
+
+        dataSets = new ArrayList<>();
+        BarDataSet set1;
+
+        set1 = new BarDataSet(usageEntries, "Energy Usage");
+        set1.setColors(ColorTemplate.MATERIAL_COLORS);
+        set1.setValueTextSize(16f);
+        dataSets.add(set1);
+
+        //String setter in x-Axis
+        barChart.getXAxis().setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
+
+        BarData data = new BarData(dataSets);
+        barChart.setData(data);
+        barChart.setFitBars(true);
+        barChart.getDescription().setText("Energy Usage per day!");
+        barChart.animateY(2000);
+
+
+    }
+
+    private List<BarEntry> getUsageEntries() {
+        ArrayList<BarEntry> energyUsage = new ArrayList<>();
+
+        energyUsage.add(new BarEntry(0, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(1, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(2, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(3, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(4, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(5, (float) getDailyUsage()));
+        energyUsage.add(new BarEntry(6, (float) getDailyUsage()));
+
+        return energyUsage.subList(0, 6);
+    }
+
+    public double getDailyUsage() {
+        double rangeMin = 6.3;
+        double rangeMax = 9.3;
+
+        Random r = new Random();
+        double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+        return randomValue;
+
     }
     public void ClickMenu(View view){
         MainActivity.openDrawer(drawerLayout);
